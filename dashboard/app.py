5545d0cf980b391dfab419793f44a0c38fa359c6
Smiai Nour <nrsm.301@gmail.com>
2025-04-20 22:42:33 +0100

    style: changed window logo

--- dashboard/app.py
+++ dashboard/app.py
@@ -79,7 +79,7 @@ app.index_string = """
         {%metas%}
         {%css%}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-        <link rel="icon" href="/assets/logos/favicon.svg" type="image/x-icon">
+        <link rel="icon" href="/assets/logos/favicon.png" type="image/x-icon">
     </head>
     <body>
         {%app_entry%}
